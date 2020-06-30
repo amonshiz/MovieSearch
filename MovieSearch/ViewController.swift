3a5f10cc8258f7c40850b25b938de3914e6ab2ff
Andrew Monshizadeh <amonshiz@gmail.com>
2020-06-30 10:20:49 -0400

    Initial Commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Andrew Monshizadeh on 6/30/20.
+//  Copyright © 2020 Andrew Monshizadeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
